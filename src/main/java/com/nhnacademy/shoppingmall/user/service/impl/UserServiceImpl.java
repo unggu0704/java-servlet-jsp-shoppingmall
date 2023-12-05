2cbd6ce0f48b6d6b678ffec14d5458428c46654b
UNGGU0704 <130115689+UNGGU0704@users.noreply.github.com>
2023-12-05 17:50:20 +0900

    feat : userRepository 방식 구현

--- src/main/java/com/nhnacademy/shoppingmall/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/nhnacademy/shoppingmall/user/service/impl/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package com.nhnacademy.shoppingmall.user.service.impl;
 
+import com.nhnacademy.shoppingmall.user.exception.UserAlreadyExistsException;
+import com.nhnacademy.shoppingmall.user.exception.UserNotFoundException;
 import com.nhnacademy.shoppingmall.user.service.UserService;
 import com.nhnacademy.shoppingmall.user.domain.User;
 import com.nhnacademy.shoppingmall.user.repository.UserRepository;
+import java.time.LocalDateTime;
 
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
@@ -14,29 +17,45 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getUser(String userId){
         //todo#4-1 회원조회
-        return null;
+        return userRepository.findById(userId).orElse(null);
     }
 
     @Override
     public void saveUser(User user) {
         //todo#4-2 회원등록
-
+        if (userRepository.countByUserId(user.getUserId()) == 0)
+            userRepository.save(user);
+        else
+            throw new UserAlreadyExistsException("유저 ID가 존재합니다.");
     }
 
     @Override
     public void updateUser(User user) {
         //todo#4-3 회원수정
+        if (userRepository.countByUserId(user.getUserId()) == 1)
+            userRepository.update(user);
+        else
+            throw new UserNotFoundException("업데이트 할려는 유저가 없습니다.");
     }
 
     @Override
     public void deleteUser(String userId) {
         //todo#4-4 회원삭제
+        if (userRepository.countByUserId(userId) == 1)
+            userRepository.deleteByUserId(userId);
+        else
+            throw new UserNotFoundException("삭제할려는 유저가 없습니다.");
     }
 
     @Override
     public User doLogin(String userId, String userPassword) {
         //todo#4-5 로그인 구현, userId, userPassword로 일치하는 회원 조회
-        return null;
+        User user = userRepository.findByUserIdAndUserPassword(userId, userPassword).orElse(null);
+        if (user == null)
+            throw new UserNotFoundException("아이디와 비밀번호가 맞지 않습니다.");
+        else
+            userRepository.updateLatestLoginAtByUserId(userId, LocalDateTime.now());
+        return user;
     }
 
 }
